603cf70d1e53922d67fd8f0eed9ac79d2c1562ab
Aleksandr Kucherenko <g28xyz@ya.ru>
2022-01-10 19:38:51 +0700

    заготовлены конфиги

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,18 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-  entry: "./src/index.tsx",
+  entry: "./src/pages/index.tsx",
   target: "web",
   mode: "development",
+  devServer: {
+    static: path.resolve(__dirname, "./dist"),
+    compress: true,
+    port: 5050,
+    open: false,
+  },
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "main.js",
@@ -25,8 +32,19 @@ module.exports = {
         loader: "source-map-loader",
       },
       {
+        test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
+        type: "asset/resource",
+      },
+      {
         test: /\.css$/,
-        loader: "css-loader",
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: { importLoaders: 1 },
+          },
+          "postcss-loader",
+        ],
       },
     ],
   },
@@ -34,8 +52,7 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, "src", "index.html"),
     }),
-    new MiniCssExtractPlugin({
-      filename: "./src/yourfile.css",
-    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin(),
   ],
 };
